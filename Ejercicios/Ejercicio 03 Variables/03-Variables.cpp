62cfeb4e871e65a47e33e4b47d6dedef9f91bf4b
KevinVz14 <kevinomarvasquezhernandez@gmail.com>
2022-06-04 17:30:35 -0600

    Ejercicios

---
+++ Ejercicios/Ejercicio 03 Variables/03-Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()  {
+		
+	int numeroEntero;
+	numeroEntero= 15;
+	
+	cout<<"Entrero: " <<numeroEntero<<endl;
+	
+	numeroEntero=18;
+	
+	cout<<"Entrero modificado: "<<numeroEntero;
+
+
+return 0;
+}
