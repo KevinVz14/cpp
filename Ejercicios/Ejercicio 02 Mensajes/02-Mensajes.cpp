5e816371bfeb5e50a72db64e9f2cf985197477bf
KevinVz14 <kevinomarvasquezhernandez@gmail.com>
2022-06-04 15:41:17 -0600

    Ejercicios

---
+++ Ejercicios/Ejercicio 02 Mensajes/02-Mensajes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()  {
+
+string nombre;
+
+cout<<" Ingre su nombre: ";
+cin >>nombre;
+
+cout<<endl;
+cout<<" Hola mi nombre es " << nombre;
+cout<<endl;
+return 0;
+}
