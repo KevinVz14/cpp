17bcbbf533de0fb7d8519f1de905b81ffc474388
KevinVz14 <kevinomarvasquezhernandez@gmail.com>
2022-07-04 22:13:23 -0600

    Ejercicios

---
+++ Ejercicios II/Ejercicio 55-Arreglos-Unidimensionales-de-tipo-string/55-Arreglos-Unidimensionales-de-tipo-string.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	
+	int numeros[] = {4,5,8};
+	
+	for (int i = 0; i<3; i++ ){
+		
+		
+		cout << numeros[i] <<endl;
+		
+		
+	}
+	
+	
+	
+
+string nombres[] = { "Juan", "Maria", "Pedro", "Jose", "Pablo" };
+
+
+for (int i = 0; i < 5; i++){
+	
+	
+	
+	cout<< nombres[i] << endl;
+}
+
+return 0;
+}
