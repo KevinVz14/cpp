e0a039e49fef5b13222af1c33590145a0195150e
KevinVz14 <kevinomarvasquezhernandez@gmail.com>
2022-07-22 08:41:53 -0600

    Ejercicios

---
+++ Ejercicios III/Ejercicio 58-Ejercicio-encuesta/58-Ejercicio-encuesta.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char const *argv[])
+{
+	
+	string alumnos[3];
+	
+	string encuesta[3][2] = {
+	
+	{"Llevara clases el siguiente periodo?", "" },
+	{"Matriculara L2?", ""},
+	{"Matriculara L2 con el lic Bily?", ""}
+
+	};
+		
+	for (int i = 0; i < 3; i ++)
+{
+	
+	cout<<"Ingrese su nombre: ";
+	cin>>alumnos[i];
+	
+	cout<<endl;
+	
+	
+	for (int j = 0; j < 3; j++ ){
+		
+		cout<<encuesta[j][0];
+		cin  >> encuesta[j][1];
+	}
+	
+}	
+		
+	return 0;
+}
